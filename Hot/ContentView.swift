25bdc9135d86db5531b5aa0b223451d059412a96
BunChan <stephenjamesfitzgerald@yahoo.com>
2025-05-31 13:27:00 +0900

    Initial Commit

---
+++ Hot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hot
+//
+//  Created by STEPHEN FITZGERALD on 2025/05/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hot/HotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotApp.swift
+//  Hot
+//
+//  Created by STEPHEN FITZGERALD on 2025/05/31.
+//
+
+import SwiftUI
+
+@main
+struct HotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
